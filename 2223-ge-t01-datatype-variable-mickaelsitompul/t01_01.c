1aac327ac6a8c002acceeff6cf682f2cc33eb695
herisitompul <135317357+herisitompul@users.noreply.github.com>
2023-06-05 11:41:53 +0700

    Add files via upload

---
+++ 2223-ge-t01-datatype-variable-mickaelsitompul/t01_01.c
@@ -0,0 +1,25 @@
+// NIM - Name
+// NIM - Name
+
+#include <stdio.h>
+
+int main(int _argc, char **_argv)
+{
+  int numerik, boeelan, unnegative;
+  char acii;
+  float fraction;
+  
+  scanf("%d\n", &numerik);
+  scanf("%c\n", &);
+  scanf("%d\n", &boeelan);
+  scanf("%d\n", &unnegative);
+  scanf("%f", &fraction);
+
+  printf("%d\n", numerik);
+  printf("%c\n", acii);
+  printf("%d\n", boeelan);
+  printf("%d\n", unnegative);
+  printf("%.2f\n", fraction);
+
+  return 0;
+}
